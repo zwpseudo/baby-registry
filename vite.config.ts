31f114c12e5b96c86ff9397d3eac982e8e385e32
zwpseudo <zackrey@mail.com>
2025-03-24 15:24:38 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/babywatford/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
@@ -14,3 +14,4 @@ export default defineConfig({
     sourcemap: false,
   },
 });
+
